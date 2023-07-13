4da3856c369410faf45cdecbc457b0535fe7c3ec
Mouaad Es-safryouy <mouaadessafryouy1@gmail.com>
2023-07-13 20:15:35 +0100

    more routes for employee

--- server/controllers/employeurController.js
+++ server/controllers/employeurController.js
@@ -54,3 +54,124 @@ export const getEmp = (req, res) => {
     return res.status(200).json(data);
   });
 };
+
+export const getEmpById = (req, res) => {
+  const immatricule = req.params.id;
+  const q = `
+  SELECT *
+  FROM employeur
+  WHERE immatricule = ?
+`;
+  db.query(q, immatricule, (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur selection employeur");
+    } else if (results.length === 0) {
+      res.status(404).send("Employeur n'exist pas");
+    } else {
+      res.send(results[0]);
+    }
+  });
+};
+
+export const updateEmp = (req, res) => {
+  const immatricule = req.params.id;
+  const {
+    prenom,
+    nom,
+    dateN,
+    sexe,
+    adresse,
+    tel,
+    email,
+    isAdmin,
+    idBureau,
+    idDes,
+    idGrade,
+  } = req.body;
+  const q = `
+  UPDATE employeur
+  SET
+    prenom = ?,
+    nom = ?,
+    dateN = ?,
+    sexe = ?,
+    adresse = ?,
+    tel = ?,
+    email = ?,
+    isAdmin = ?,
+    idBureau = ?,
+    idDes = ?,
+    idGrade = ?
+  WHERE immatricule = ?
+`;
+  const values = [
+    prenom,
+    nom,
+    dateN,
+    sexe,
+    adresse,
+    tel,
+    email,
+    isAdmin ? 1 : 0,
+    idBureau,
+    idDes,
+    idGrade,
+    immatricule,
+  ];
+
+  db.query(q, values, (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur de mise a jour");
+    } else {
+      res.send("Employeur et mis a jour");
+    }
+  });
+};
+
+export const deleteEmp = (req, res) => {
+  const immatricule = req.params.id;
+  const q = `
+    DELETE FROM employeur
+    WHERE immatricule = ?
+  `;
+  db.query(q, immatricule, (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur suppression");
+    } else {
+      res.send("Employeur supprimer");
+    }
+  });
+};
+
+export const getEmpDesignation = (req, res) => {
+  const q = `
+  SELECT e.immatricule, e.prenom, e.nom, d.nomDes
+  FROM employeur e
+  JOIN designation d ON e.idDes = d.idDes
+
+`;
+
+  db.query(q, (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur designation");
+    } else {
+      res.send(results);
+    }
+  });
+};
+
+export const getEmpBureau = (req, res) => {
+  const q = `
+  SELECT e.immatricule, e.prenom, e.nom, b.intitule, b.etagere, b.idSd
+  FROM employeur e
+  JOIN bureau b ON e.idBureau = b.idBureau
+`;
+
+  db.query(q, (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur bureau emp");
+    } else {
+      res.send(results);
+    }
+  });
+};
