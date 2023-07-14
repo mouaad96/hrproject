b99bbd59ece7d3fb832ea353af159e87e9b4e39d
Mouaad Es-safryouy <mouaadessafryouy1@gmail.com>
2023-07-14 16:40:01 +0100

    Revert "Revert "more routes for employee""
    This reverts commit f52cd74e4a0bd347967dc6f6c3d342b93b9bae61.

--- server/routes/employeurRoute.js
+++ server/routes/employeurRoute.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { addEmp, getEmp } from "../controllers/employeurController.js";
+import {
+  addEmp,
+  deleteEmp,
+  getEmp,
+  getEmpBureau,
+  getEmpById,
+  getEmpDesignation,
+  updateEmp,
+} from "../controllers/employeurController.js";
 
 const router = express.Router();
 
 router.post("/create", addEmp);
 router.get("/list", getEmp);
+router.get("/employeur/:id", getEmpById);
+router.get("/empDes", getEmpDesignation);
+router.get("/empBur", getEmpBureau);
+router.put("/update/:id", updateEmp);
+router.delete("/delete/:id", deleteEmp);
 
 export default router;
