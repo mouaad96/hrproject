aca93770d9e64a03b19803db1b469476adbd99f5
Mouaad Es-safryouy <81655883+mouaad96@users.noreply.github.com>
2024-09-24 19:51:33 +0100

    Update connect.js

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,9 +1,9 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "admin",
-  port: "3307",
-  database: "hrsystem",
+  host: "",
+  user: "",
+  password: "",
+  port: "",
+  database: "",
 });
